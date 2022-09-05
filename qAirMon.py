78c2ba54d82da89fd83b5a4d4884c5b890d168db
Mixarius <mixarius@gmail.com>
2022-09-05 14:39:10 +0200

    The last update time and address show in the menu. Last update time is clickable and get air quality. Current coordinates in the menu is clickable and can set new coordinates for monitoring.

--- qAirMon.py
+++ qAirMon.py
@@ -15,18 +15,59 @@ LEVEL_TYPES = {
     'AIRMAGEDDON':  'data/airmageddon.png'
 }
 UA = user_agent("safari")
-UPDATED = "Updated"
-ADDRESS = "Address"
+UPDATED = "⏳"
+ADDRESS = "🏠"
+CURRENT_COORDINATES = '⛳'
+SET_COORDINATES = {
+    'title': f'⚙️Set coordinates',
+    'message': f'Set the coordinates where you want to monitor the air.\n '
+               f'Copy the coordinates into Google Maps and paste them here.'
+}
 
 
 class QAirMonApp(rumps.App):
     url = 'https://widget.airly.org/api/v1/'
     current_level = LEVEL_TYPES['OFFLINE']
+    latitude = '52.2394646242'  # https://airly.org/map/en/#52.2394646242,21.0457174815
+    longitude = '21.0457174815'
 
     @rumps.clicked(UPDATED)
     @rumps.timer(60)
-    def get_air_quality(self, sender) -> None:
+    def refresh_status(self,_):
+        """Refresh AIRLY CAQI information on menu."""
+        self.menu[CURRENT_COORDINATES].title = f'{CURRENT_COORDINATES}: {self.latitude}, {self.longitude}'
+        response = self.get_air_quality(None)
+        if response:
+            self.icon = response['level']
+            self.menu[UPDATED].title = f'{UPDATED}: {response["updated"]}'
+            self.menu[ADDRESS].title = f'{ADDRESS}: {response["address"]}'
+
+    @rumps.clicked(CURRENT_COORDINATES)
+    def set_coordinates(self, _):
+        """ Set address coordinates for monitoring  """
+        setting_window = rumps.Window(
+            title=SET_COORDINATES['title'],
+            message=SET_COORDINATES['message'],
+            default_text=f'{self.latitude}, {self.longitude}',
+            ok='Save',
+            cancel='Cancel',
+        )
+
+        response = setting_window.run()
+        if response.clicked:
+            latitude, longitude = str(response.text).strip().split(', ')
+            self.latitude = latitude
+            self.longitude = longitude
+
+            self.refresh_status(None)
+
+    def get_air_quality(self, _) -> dict:
         """ Get air quality from Airly API """
+        result = dict()
+
+        if not self.latitude or not self.longitude:
+            return result
+
         headers = {
             'Origin': 'https://airly.org',
             'Accept-Encoding': 'gzip, deflate, br',
@@ -35,10 +76,11 @@ class QAirMonApp(rumps.App):
             'Accept-Language': 'en-gb',
             'Referer': 'https://airly.org/',
         }
+
         params = {
             'displayMeasurements': 'false',
-            'latitude': '52.2394646242', # https://airly.org/map/en/#52.2394646242,21.0457174815
-            'longitude': '21.0457174815',
+            'latitude': self.latitude,
+            'longitude': self.longitude,
             'id': 'null',
             'indexType': 'AIRLY_CAQI',
             'language': 'en',
@@ -51,14 +93,12 @@ class QAirMonApp(rumps.App):
             if response and response.status_code == 200:
                 json_data = response.json()
 
-                level = json_data['level']
-                self.icon = LEVEL_TYPES[level]
+                result['level'] = LEVEL_TYPES[json_data['level']]
+                result['address'] = json_data["address"]
 
                 datetime_object = datetime.strptime(json_data['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
-                datetime_str = datetime_object.strftime('%y-%m-%d %H:%M:%S')
-                self.menu[UPDATED].title = f'{UPDATED}: {datetime_str}'
-
-                self.menu[ADDRESS].title = f'{ADDRESS}: {json_data["address"]}'
+                datetime_str = datetime_object.strftime('%Y-%m-%d %H:%M:%S')
+                result['updated'] = datetime_str
 
         except requests.exceptions.HTTPError as errh:
             print("Http Error:", errh)
@@ -69,6 +109,7 @@ class QAirMonApp(rumps.App):
         except requests.exceptions.RequestException as err:
             print("OOps: Something Else", err)
 
+        return result
 
 if __name__ == "__main__":
     app = QAirMonApp("Quality Air Monitor", title=None, icon='data/offline.png')
@@ -76,7 +117,7 @@ if __name__ == "__main__":
         UPDATED,
         ADDRESS,
         None,
-        "Preferences",
+        CURRENT_COORDINATES,
         None,
     ]
     rumps.debug_mode(True)
